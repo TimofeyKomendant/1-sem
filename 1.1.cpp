bdbae647cddc4fd9d5ac848d402b45a4d33271b3
TimofeyKomendant <78606697+TimofeyKomendant@users.noreply.github.com>
2021-02-05 18:13:27 +0300

    Add files via upload

---
+++ 1.1.cpp
@@ -0,0 +1,45 @@
+﻿#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double a, b, z1, z2;
+	cout << "введите первое число " << endl;
+	while (true)
+	{
+		cin >> a;
+		if (!cin.fail())
+		{
+			break;
+		}
+		cin.clear();
+		cin.ignore(32767, '\n');
+		cout << "попробуй ещё раз" << endl;
+	}
+	cout << "введите второе число " << endl;
+	while (true)
+	{
+		cin >> b;
+		if (!cin.fail())
+		{
+			break;
+		}
+		cin.clear();
+		cin.ignore(32767, '\n');
+		cout << "попробуй ещё раз" << endl;
+	}
+	z1 = (sqrt(2 * a + 2 * sqrt(a * a - 4))) / (sqrt(a * a - 4) + a + 2);
+	z2 = 1 / (sqrt(a + 2));
+	cout << "z1=" << z1 << endl << "z2=" << z2 << endl;
+	if (z1 == z2)
+	{
+		cout << "всё в порядке, значения равны" << endl;
+		return 0;
+	}
+	else
+	{
+		cout << "ошибка, значения не равны" << endl;
+		return 0;
+	}
+}
